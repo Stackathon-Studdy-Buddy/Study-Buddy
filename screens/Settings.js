73f6778731f232fc500f1b02e6d5971af6ef131d
Celia Macrae <celiamacrae@gmail.com>
2020-01-27 11:20:43 -0500

    merging

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,19 +1,51 @@
 import React from 'react';
+import {connect} from 'react-redux'
 import { StyleSheet, Text, View,Button } from 'react-native';
+import UpdateUserProfileScreen from './UpdateProfileForm'
+import {updateProfile} from '../store/user'
+import * as Font from 'expo-font'
 
-export default class Settings extends React.Component{
+class SettingsScreen extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+
+    }
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'bangers': require('../assets/fonts/Bangers-Regular.ttf')
+    })
+  }
 
   render(){
+    console.log("SETTINGS", this.props)
 
     return(
         <View style={styles.container}>
-          <Text>Settings</Text>
-          <Button title="Back"
-          onPress={()=>this.props.navigation.navigate('Home')}/>
+
+          <View style={styles.update}>
+            <Text style={styles.fon}>Update Profile: </Text>
+          </View>
+
+          <View style={styles.inner}>
+
+
+
+              <UpdateUserProfileScreen user={this.props.user} onUpdateProfile={this.props.onUpdateProfile}/>
+
+          </View>
+
+          <View style={styles.back}>
+            <Button title="Back"
+            onPress={()=>this.props.navigation.navigate('Home')}/>
+          </View>
         </View>
     )
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -21,4 +53,40 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  back: {
+    position:"absolute",
+    top: "13%",
+  },
+  update: {
+    position:"absolute",
+    top: "30%",
+  },
+  inner: {
+    position:"absolute",
+    top: "35%",
+    left: "25%"
+  },
+  fon: {
+    fontSize:20,
+    fontFamily:"bangers"
+  }
 });
+
+const mapStateToProps = function(state) {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = function(dispatch) {
+  return {
+    onUpdateProfile: function(user){
+      const thunk = updateProfile(user)
+      dispatch(thunk)
+    }
+  }
+}
+
+const Settings = connect(mapStateToProps, mapDispatchToProps)(SettingsScreen)
+
+export default Settings
--- screens/UserProfile.js
+++ screens/UserProfile.js
@@ -3,19 +3,55 @@ import {connect} from 'react-redux'
 import { StyleSheet, Text, View,Button,TextInput,ImageBackground ,Image} from 'react-native';
 import {loadUser, updateProfile} from '../store/user'
 import UpdateUserProfileScreen from './UpdateProfileForm'
+import * as Font from 'expo-font'
+
+const image = require('../public/blank.png')
 
 class UserProfileScreen extends React.Component{
 
+  async componentDidMount() {
+    await Font.loadAsync({
+      'bangers': require('../assets/fonts/Bangers-Regular.ttf')
+    })
+  }
+
   render(){
     console.log("USER PROFILE PROPS", this.props)
+    console.log("MEETINGS", this.props.meetings)
     return(
       <View style={styles.container}>
-        <Text>PROFILE:</Text>
-        <Text>{this.props.user.firstName} {this.props.user.lastName}</Text>
-        <Text>email: {this.props.user.email}</Text>
+        <Text>{'\n'}</Text>
+        <Text style={styles.fon}>{this.props.user.firstName} {this.props.user.lastName}</Text>
 
-        <Text>Update Profile: </Text>
-        <UpdateUserProfileScreen user={this.props.user} onUpdateProfile={this.props.onUpdateProfile}/>
+        <Image
+          style={{width: 100, height: 100}}
+          source={image}
+        />
+
+        <Text>email: {this.props.user.email}{"\n"}</Text>
+
+        {/* <Text>Update Profile: </Text>
+        <View>
+          <UpdateUserProfileScreen user={this.props.user} onUpdateProfile={this.props.onUpdateProfile}/>
+        </View> */}
+
+
+        <Text style={styles.fon}>{"\n"}My Meetings:{"\n"}</Text>
+        <View style={styles.allMeetings}>
+          {this.props.meetings.map(meeting => {
+            const date= new Date(meeting.data.date._seconds * 1000).toString().slice(0,10).trim()
+            const hour=new Date(meeting.data.date._seconds * 1000).getHours()
+            const minute=new Date(meeting.data.date._seconds * 1000).getMinutes()
+            if(meeting.data.user === this.props.user.email){
+              return (
+                <View style={styles.meeting} key={meeting.id}>
+                  <Text>{date}</Text>
+                  <Text>{meeting.data.name}</Text>
+                </View>
+              )
+            }
+          })}
+        </View>
 
 
       </View>
@@ -29,8 +65,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  meeting: {
+    padding: '3%',
+    borderRadius: 4,
+    borderWidth: 0.5,
+  },
+  fon : {
+    fontSize:30,
+    fontFamily: 'bangers'
+  },
+  allMeetings: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    padding: '3%'
+  }
 });
 
 
